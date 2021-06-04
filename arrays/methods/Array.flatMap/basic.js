c4c2e41265f2b29042c21fa0a241b4c892edec06
Ajay Kumbhare <ajaykumbhare@outlook.com>
2021-06-04 10:44:36 +0530

    flatMap console issue fixed

--- arrays/methods/Array.flatMap/basic.js
+++ arrays/methods/Array.flatMap/basic.js
@@ -4,5 +4,8 @@ Maps and then returns a flattened array
 
 const num = [1, [2], 4];
 
-console.log(num.flatMap((e) => e * 2));
+console.log(num.flatMap((e) => e));
 // [ 1, 2, 4 ]
+
+console.log(num.flatMap((e) => e * 2));
+// [ 2, 4, 8 ]
