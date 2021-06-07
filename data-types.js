9cdb369611714d5d3b53142e54c2437b657f0d21
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2021-06-07 21:23:45 +0530

    Added some examples

--- data-types.js
+++ data-types.js
@@ -1,10 +1,10 @@
 /*
     ? Primitive types
-
-    String
-    Number
-    BigInt
-    Boolean
+    
+    String- "manoj"
+    Number- 37,-9.25
+    BigInt  - represent whole numbers larger than 2^53 - 1
+    Boolean - true,false
     Undefined
     Null
     Symbol
