4ad4dd237331fe7928f9e51822309a7978fefba6
Siddhi Bhanushali <69195262+siddhi-244@users.noreply.github.com>
2021-06-08 08:28:55 +0530

    Updated definitions

--- data-types.js
+++ data-types.js
@@ -1,18 +1,18 @@
 /*
     ? Primitive types
     
-    String- "manoj"
-    Number- 37,-9.25
+    String- represents sequence of characters e.g. "ram"
+    Number-   represents numeric values eg 37,46 
     BigInt  - represent whole numbers larger than 2^53 - 1
-    Boolean - true,false
-    Undefined
-    Null
-    Symbol
+    Boolean - represents boolean value either false or true
+    Undefined - represents undefined value
+    Null - represents null i.e. no value at all
+    Symbol- Symbols are often used to add unique property keys to an object
 */
 
 /*
     ? Non - primitive types
 
-    Object
-    Arrays
+    Object - instance through which we can access members.
+    Arrays - represents group of similar values
 */
