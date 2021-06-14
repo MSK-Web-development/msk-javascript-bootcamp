e4bd6b2c6226c44aae525a080d6b33756b8448cd
geekySapien <80349103+geekySapien@users.noreply.github.com>
2021-06-14 12:10:35 +0530

    WindowObject.js

---
+++ concepts/WindowObject.js
@@ -0,0 +1,14 @@
+
+console.log("This is the tutorial to explore window object in javascript.");
+console.log(`Windows object is by default a global object of client side.`);
+let a = window;
+a = prompt("Enter your name");
+let b = confirm("Are you sure you want to submit the solution");
+let c = innerHeight;
+let d = innerWidth;
+let e = location;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
