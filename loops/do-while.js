1b37565999818f925e24e51f20ea12d95b865449
Arth Gupta <72153219+guptaarth87@users.noreply.github.com>
2021-10-01 02:34:45 +0530

    Update do-while.js
    Do while explanation with example

--- loops/do-while.js
+++ loops/do-while.js
@@ -1,3 +1,20 @@
+/*
+do-while executes command atleast once and then checks the condition
+do block gets execute and then it checks the condition
+*/
+
+//syntax
 do {
     
 } while (condition);
+
+//example
+var a=4
+do{
+    console.log(a);  
+    a=a-1
+}while(a>5);
+
+/*output
+4
+*/
