61a9f5c0f22f103d8f018c7ba31ea15dff21bf6d
Suraj Singh <55339137+Suraj7879@users.noreply.github.com>
2021-10-11 09:43:44 +0530

    added example for do-while loop

--- loops/do-while.js
+++ loops/do-while.js
@@ -1,3 +1,12 @@
+/* The difference between while and do while loop is, the do-while loop are always executed atleast once */
 do {
     
 } while (condition);
+
+example:-
+    let i = 0;
+    do{
+        if (i % 2 !== 0) console.log(i);
+        i++;
+    } while (i <= 5);
+
