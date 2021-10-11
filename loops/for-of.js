dea2602ced5272d84912746d86ad7975904888fe
Suraj Singh <55339137+Suraj7879@users.noreply.github.com>
2021-10-11 09:49:47 +0530

    added example of for-of loop

--- loops/for-of.js
+++ loops/for-of.js
@@ -1,2 +1,19 @@
 for (variable of object)
   statement
+
+example:-
+  /*for-in loop in object*/
+  const person = {
+    name: 'Suraj',
+    age: 18
+  };
+
+  for (let key in person)
+    console.log(key, person[key])
+
+
+  /*for-in loop in array*/
+  const colors = ['red', 'green', 'blue'];
+  
+  for (let index in colors)
+    console.log(index)
