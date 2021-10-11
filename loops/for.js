be37353e5cadc1ea48fbd78b58f25b3518eab320
Suraj Singh <55339137+Suraj7879@users.noreply.github.com>
2021-10-11 09:35:10 +0530

    for loop example

--- loops/for.js
+++ loops/for.js
@@ -2,3 +2,8 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
 }
+
+Example :-
+    for(let i = 1; i<=5; i++) {
+        console.log('Hello World', i)
+    }
